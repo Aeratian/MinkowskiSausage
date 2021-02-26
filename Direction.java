726fc4442193ddadf71d6ee5c9097adcd86522c6
Ian Youn <74224325+Aeratian@users.noreply.github.com>
2021-02-25 19:27:02 -0800

    Add files via upload

---
+++ Direction.java
@@ -0,0 +1,116 @@
+/**
+ * Direction.java
+ * 
+ * A class for representing the direction of a line segment. The xDir 
+ * represents the multiplier for the x-component of the line and the yDir
+ * represents the multiplier for the y-component of the line. The sum
+ * of the squares of xDir and yDir will always equal 1. Includes two 
+ * constructors and getters and setters for the xDir and yDir variables.
+ * The class also include two methods turnLeft and turnRight for turning
+ * 90 degrees left and right.
+ * 
+ * @author Ian Youn
+ * @version 1.0
+ * @since 2/25/2021
+ */
+public class Direction 
+{
+	/** The multiplier for the x-component of the direction. */
+	private double xDir;
+
+	/** The multiplier for the y-component of the direction. */
+	private double yDir;
+
+	/**
+	 * A no-args constructor for creating a Direction object of 
+	 * a horizontal line pointing towards the positive x-direction.
+	 */
+	public Direction()
+	{
+		xDir = 1.0;
+		yDir = 0.0;
+	}
+
+	/**
+	 * A two-args constructor for creating a Direction object, The
+	 * parameters represent the x-component and y-component of the
+	 * direction.
+	 * @param xD 		The x-component of the direction.
+	 * @param yD		The y-component of the direction.
+	 */
+	public Direction(double xD, double yD)
+	{
+	    xDir = xD;
+	    yDir = yD;
+	}
+
+	/**
+	 * Gets the x-component of the Direction.
+	 * @return			The x-component of the Direction.
+	 */
+	public double getxDir() 
+	{
+	    return xDir;
+	}
+
+	/**
+	 * Sets the x-component of the direction.
+	 * @param xD		The new x-component of the Direction.
+	 */
+	public void setxDir(double xD) 
+	{
+	    xDir = xD;
+	}
+
+	/**
+	 * Gets the y-component of the Direction.
+	 * @return			The y-component of the Direction.
+	 */
+	public double getyDir() 
+	{
+	    return yDir;
+	}
+
+	/**
+	 * Sets the x-component of the direction.
+	 * @param yD		The new y-component of the Direction.
+	 */
+	public void setyDir(double yD) 
+	{
+	    yDir = yD;
+	}
+
+	/**
+	 * Turns the Direction object 90 degrees to the left.
+	 */
+	public void turnLeft()
+	{
+		if(xDir == 0)
+		{
+			xDir = -yDir;
+			yDir = 0;
+		}
+		else
+		{
+			yDir = xDir;
+			xDir = 0;
+		}
+	}
+
+	/**
+	 * Turns the Direction object 90 degrees to the right.
+	 */
+	public void turnRight()
+	{
+		if(xDir == 0)
+		{
+			xDir = yDir;
+			yDir = 0;
+		}
+		else
+		{
+			yDir = -xDir;
+			xDir = 0;
+		}
+	}
+}
---
+++ Showpiece.java
@@ -0,0 +1,92 @@
+/**
+ * Showpiece.java
+ * 
+ * A class for drawing a fractal-pattern called the "Minkowski sausage" using 
+ * recursion. A number can be passed into the arguments to signify the depth 
+ * of the pattern. Otherwise, the program is run with a depth of 3.
+ * 
+ * @author Ian Youn
+ * @version 1.0
+ * @since 2/24/2021
+ */
+import java.awt.Color;
+
+public class Showpiece
+{
+	/**
+	 * The main method of the program.
+	 * @param args		The array of arguments containing the depth of the pattern.
+	 */
+	public static void main(String [] args)
+	{
+		setUpAndDrawBackground();
+		int n = 3;
+		if(args.length > 0)
+		{
+			n = Integer.parseInt(args[0]);
+		}
+		quadKoch(n, -450, 0, new Direction(1, 0), 900);
+	}
+	
+	/**
+	 * Sets up the background and scale of the StdDraw canvas.
+	 */
+	public static void setUpAndDrawBackground ( )
+	{
+		StdDraw.setCanvasSize(900, 900);
+		StdDraw.setXscale(-450, 450);
+		StdDraw.setYscale(-450, 450);
+	}
+
+	/**
+	 * A recursive method for drawing a fractal pattermn. In this fractal pattern,
+	 * every straight line is changed with a square of length length/4 at the 
+	 * top-middle-left and a square of length length/4 at the bottom-middle-right
+	 * (use the program with argument 1 for a better explanation), starting from 
+	 * a straight line.                                              
+	 * @param n 		The depth of the line (starts at max, ends at 0).
+	 * @param x0		The starting x-coordinate of the line.
+	 * @param y0		The starting y-coordinate of the line.
+	 * @param d			The direction of the line.
+	 * @param length	The length of the line.
+	 */
+	public static void quadKoch(int n, double x0, double y0, Direction d, int length)
+	{
+		if(n > 0)
+		{
+			StdDraw.setPenColor(new Color(10,  100 + (int)(Math.random() * 100), 100 + (int)(Math.random() * 100)));
+			quadKoch(n - 1, x0, y0, d, length / 4);
+			x0 += length / 4 * d.getxDir();
+			y0 += length / 4 * d.getyDir();
+			d.turnLeft();
+			quadKoch(n - 1, x0, y0, d, length / 4);
+			x0 += length / 4 * d.getxDir();
+			y0 += length / 4 * d.getyDir();
+			d.turnRight();
+			quadKoch(n - 1, x0, y0, d, length / 4);
+			x0 += length / 4 * d.getxDir();
+			y0 += length / 4 * d.getyDir();
+			d.turnRight();
+			quadKoch(n - 1, x0, y0, d, length / 4);
+			x0 += length / 4 * d.getxDir();
+			y0 += length / 4 * d.getyDir();
+			quadKoch(n - 1, x0, y0, d, length / 4);
+			x0 += length / 4 * d.getxDir();
+			y0 += length / 4 * d.getyDir();
+			d.turnLeft();
+			quadKoch(n - 1, x0, y0, d, length / 4);
+			x0 += length / 4 * d.getxDir();
+			y0 += length / 4 * d.getyDir();
+			d.turnLeft();
+			quadKoch(n - 1, x0, y0, d, length / 4);
+			x0 += length / 4 * d.getxDir();
+			y0 += length / 4 * d.getyDir();
+			d.turnRight();
+			quadKoch(n - 1, x0, y0, d, length / 4);
+		}
+		else
+		{
+			StdDraw.line(x0, y0, x0 + length * d.getxDir(), y0 + length * d.getyDir());
+		}
+	}
+}
